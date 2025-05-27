477702e36faf6cde31139fb6e078de8b61f2cd76
JaebeomCho <aspire1019@snu.ac.kr>
2025-05-27 13:18:10 +0900

    update KDEMO.jl compact

--- src/cases/KDEMO.jl
+++ src/cases/KDEMO.jl
@@ -101,53 +101,55 @@ function case_parameters(::Type{Val{:KDEMO_compact}})
     ini.general.casename = "K-DEMO_compact"
     ini.general.init_from = :scalars
 
-    gaps_thickness = 0.1
+    ini.general.dd = IMAS.dd()
+
+    gaps_thickness = 0.01
     ini.build.layers = OrderedCollections.OrderedDict(
-        :gap_OH => 0.8,
-        :OH => 0.22,
-        :hfs_TF => 0.949,
+        :gap_OH => 0.57,
+        :OH => 0.28,
+        :hfs_TF => 0.72,
         :hfs_gap_TF_vacuum_vessel => gaps_thickness,
         :hfs_vacuum_vessel_outer => 0.1,
-        :hfs_gap_water => 0.15,
+        :hfs_gap_water => 0.05,
         :hfs_vacuum_vessel_inner => 0.1,
         :hfs_gap_high_temp_shield_vacuum_vessel => 0.01,
-        :hfs_high_temp_shield => 0.2,
-        :hfs_blanket => 0.38,
+        :hfs_high_temp_shield => 0.39,
+        :hfs_blanket => 0.31,
         :hfs_first_wall => 0.02,
-        :plasma => 1.3,
+        :plasma => 1.2,
         :lfs_first_wall => 0.02,
-        :lfs_blanket => 0.7,
+        :lfs_blanket => 1.2,
         :lfs_high_temp_shield => 0.2,
         :lfs_gap_high_temp_shield_vacuum_vessel => 0.01,
         :lfs_vacuum_vessel_inner => 0.1,
-        :lfs_gap_water => 0.15,
+        :lfs_gap_water => 0.05,
         :lfs_vacuum_vessel_outer => 0.1,
-        :lfs_gap_TF_vacuum_vessel => 0.789,
-        :lfs_TF => 0.949,
-        :gap_cryostat => 1.0,
+        :lfs_gap_TF_vacuum_vessel => 0.3,
+        :lfs_TF => 0.68,
+        :gap_cryostat => 0.1,
         :cryostat => 0.1
     )
     ini.build.plasma_gap = 0.125
     ini.build.symmetric = false
     ini.build.divertors = :lower
     ini.build.n_first_wall_conformal_layers = 1
-    ini.equilibrium.B0 = 8.35        # Updated from bt
-    ini.equilibrium.R0 = 3.516       # Updated from rmajor
-    ini.equilibrium.ϵ = 0.3088235294117647
-    ini.equilibrium.κ = 1.8          # Updated from kappa
+    ini.equilibrium.B0 = 8.2        # Updated from bt
+    ini.equilibrium.R0 = 3.5       # Updated from rmajor
+    ini.equilibrium.ϵ = 0.2857
+    ini.equilibrium.κ = 2.0          # Updated from kappa
     ini.equilibrium.δ = 0.6          # Updated from triang
-    ini.equilibrium.ip = 7.4713e6    # Updated from Ip
+    ini.equilibrium.ip = 7.6e6    # Updated from Ip
     ini.equilibrium.xpoints = :lower
     ini.equilibrium.boundary_from = :scalars
 
     ini.core_profiles.ne_setting = :greenwald_fraction_ped
-    ini.core_profiles.ne_value = 0.675
+    ini.core_profiles.ne_value = 0.71   #0.71 for high performance
     ini.core_profiles.ne_shaping = 0.9
     ini.core_profiles.Te_core = 20E3
     ini.core_profiles.Te_shaping = 1.8
     ini.core_profiles.Ti_Te_ratio = 1.0
-    ini.core_profiles.zeff = 2.0
-    ini.core_profiles.helium_fraction = 0.01
+    ini.core_profiles.zeff = 2.5
+    ini.core_profiles.helium_fraction = 0.03
     ini.core_profiles.rot_core = 0.0
     ini.core_profiles.bulk = :DT
     ini.core_profiles.impurity = :Ne
@@ -156,28 +158,45 @@ function case_parameters(::Type{Val{:KDEMO_compact}})
     ini.build.layers[:gap_cryostat].coils_inside = 6
 
     ini.oh.technology = :rebco
-    ini.pf_active.technology = :rebco
+    ini.pf_active.technology = :nb3sn
     ini.tf.technology = :rebco
 
-    ini.tf.shape = :miller
+    ini.tf.shape = :double_ellipse
     ini.tf.n_coils = 18
 
     ini.center_stack.bucked = true
     ini.center_stack.plug = true
 
-    resize!(ini.ec_launcher, 1)
-    ini.ec_launcher[1].power_launched = 5.0e7
 
-    resize!(ini.ic_antenna, 1)
-    ini.ic_antenna[1].power_launched = 5.0e7
+    #resize!(ini.ec_launcher, 1)
+    #ini.ec_launcher[1].power_launched = 5.0e7
+
+    #resize!(ini.ic_antenna, 1)
+    #ini.ic_antenna[1].power_launched = 5.0e7
+    if isempty(ini.general.dd.core_sources) || !scenario_sources
+        empty!(ini.general.dd.core_sources)
+        resize!(ini.nb_unit, 1)
+        ini.nb_unit[1].power_launched = 10e7  # 60MW 고정 전력 설정
+        ini.nb_unit[1].beam_energy = 330e3
+        ini.nb_unit[1].beam_mass = 2.0
+        ini.nb_unit[1].offaxis = true
+    else
+        act.ActorHCD.nb_model = :none
+        act.ActorHCD.ec_model = :none
+        act.ActorHCD.lh_model = :none
+        act.ActorHCD.ic_model = :none
+        act.ActorHCD.pellet_model = :none
+    end
 
     ini.requirements.flattop_duration = 3000.0
-    ini.requirements.tritium_breeding_ratio = 0.8
-    ini.requirements.power_electric_net = 100e6 # as example
+    ini.requirements.tritium_breeding_ratio = 1.2
+    ini.requirements.power_electric_net = 150e6 # as example
 
     #### ACT ####
 
-    act.ActorTGLF.tglfnn_model = "sat1_em_iter"
+    act.ActorTGLF.tglfnn_model = "sat0_em_d3d"
+    act.ActorCoreTransport.model = :FluxMatcher
+    act.ActorPlasmaLimits.raise_on_breach = false
 
     return ini, act
 end
